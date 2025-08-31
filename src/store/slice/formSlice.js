9726652983272376adb8f2ebda11071d5510ae7b
Marimuthu E <marimuthue1302@gmail.com>
2025-08-31 19:28:39 +0530

    Form Preview Design Changes

--- src/store/slice/formSlice.js
+++ src/store/slice/formSlice.js
@@ -1,16 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const formSlice = createSlice({
-    name: "formBuilder",
-    initialState: [],
-    reducers: {
-        formBuilder: (state, action) => {
-            state.push({ formDetails: action.payload })
-        }
+  name: "formBuilder",
+  initialState: [],
+  reducers: {
+    formBuilder: (state, action) => {
+      state.push(action.payload);
     },
-})
-    
-export const { formBuilder } = formSlice.actions;
+    updateField: (state, action) => {
+      const index = state.findIndex(
+        (field) => field.fieldName === action.payload.fieldName
+      );
+      if (index !== -1) {
+        state[index] = action.payload; // replace existing field
+      }
+    },
+  },
+});
+
+export const { formBuilder, updateField } = formSlice.actions;
 export default formSlice.reducer;
-
-
